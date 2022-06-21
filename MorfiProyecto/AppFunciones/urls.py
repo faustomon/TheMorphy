074f85833d3792a4820dea207fccd75c7733c8b1
faustomon <105680262+faustomon@users.noreply.github.com>
2022-06-21 11:15:30 -0300

    [ADD] Se seteo la base para trabajar (Templates, URls, forms, etc

---
+++ MorfiProyecto/AppFunciones/urls.py
@@ -0,0 +1,3 @@
+from django.urls import path
+from AppFunciones import views
+from AppFunciones.views import *
--- MorfiProyecto/AppFunciones/views.py
+++ MorfiProyecto/AppFunciones/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
-
+from django.http import HttpResponse
+from django.template import loader
+from AppFunciones.forms import *
+from AppFunciones.models import *
 # Create your views here.
+def inicio(self):
+    plantilla = loader.get_template("AppFunciones/templates/inicio.html")
+    documento = plantilla.render()
+    return HttpResponse(documento)
