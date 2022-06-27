b8162769dc96b0c9fa4e24d81f6df76f5659c413
FediMont <fedemontagn@gmail.com>
2022-06-27 16:53:18 -0300

    peques ajustes

--- MorfiProyecto/AppFunciones/urls.py
+++ MorfiProyecto/AppFunciones/urls.py
@@ -3,7 +3,7 @@ from AppFunciones import views
 from AppFunciones.views import *
 
 urlpatterns = [
-    path("", inicio, name="inicio"),
+    path("", inicio, name="Inicio"),
     path("restaurantes/", restaurantes.as_view(), name="restaurantes"),
 
 ]
