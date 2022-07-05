dce3be7f6c04011732950fce15a0aeed247c2a31
faustomon <105680262+faustomon@users.noreply.github.com>
2022-07-05 19:36:53 -0300

    [ADD] Base de Datos

--- MorfiProyecto/MorfiProyecto/settings.py
+++ MorfiProyecto/MorfiProyecto/settings.py
@@ -26,7 +26,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-$gnays-q)^xl%7@b1_k48zpd34dgw#$yo4+6e#meojcv3&fto9'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False 
+DEBUG = True 
 
 ALLOWED_HOSTS = ["*"]
 
